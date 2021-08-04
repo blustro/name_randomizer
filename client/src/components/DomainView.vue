40da837b94e534fe2149b96df846b08071e936d8
Bruno Lustro <brunolustro@gmail.com>
2021-08-04 18:49:05 +0000

    Created domain extension method

--- client/src/components/DomainView.vue
+++ client/src/components/DomainView.vue
@@ -2,16 +2,75 @@
   <div>
     <div class="container">
       <router-link to="/domains">Back</router-link>
-      <br />
-      <div>
-        <h2>{{ domain }}</h2>
+      <h2 class="my-3">{{ domain }}</h2>
+      <div class="card">
+        <div class="card-body">
+          <ul class="list-group">
+            <li
+              class="list-group-item"
+              v-for="domain in domains"
+              :key="domain.extension"
+            >
+              <div class="row">
+                <div class="col-md-6">
+                  {{ domain.extension }}
+                </div>
+                <div class="col-md-3">
+                  <span
+                    class="badge"
+                    :class="[domain.available ? 'bg-success' : 'bg-danger']"
+                    >{{ domain.available ? "Available" : "Unavailable" }}</span
+                  >
+                </div>
+                <div class="col-md-3 text-end">
+                  <a
+                    class="btn btn-info"
+                    :href="domain.checkout"
+                    target="blank"
+                  >
+                    <span class="fa fa-shopping-cart"></span>
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   props: ["domain"],
+  data() {
+    return { domains: [] };
+  },
+  created() {
+    axios({
+      url: "http://localhost:4000",
+      method: "post",
+      data: {
+        query: `
+          mutation ($name: String) {
+            domains: generateDomain(name: $name) {
+              name
+              extension
+              checkout
+              available
+            }
+          }
+        `,
+        variables: {
+          name: this.domain,
+        },
+      },
+    }).then((response) => {
+      const query = response.data;
+      this.domains = query.data.domains;
+    });
+  },
 };
 </script>
